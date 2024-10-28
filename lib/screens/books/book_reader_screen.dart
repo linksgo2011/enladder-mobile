8b2cea7f01b26c740aea1eb8bd92e9d81bb009ef
Ning <120377843>
2024-10-28 20:01:46 +0800

    reafactor theme setting

--- lib/screens/books/book_reader_screen.dart
+++ lib/screens/books/book_reader_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../../models/book.dart';
 import '../../widgets/webview_reader/webview_reader.dart';
+import '../../services/book_service.dart';
+import '../../services/profile_service.dart';
+import '../../services/book_reading_service.dart';
 
 class BookReaderScreen extends StatefulWidget {
   const BookReaderScreen({super.key, required this.book});
@@ -13,8 +16,51 @@ class BookReaderScreen extends StatefulWidget {
 }
 
 class _BookReaderState extends State<BookReaderScreen> {
+  final BookService _bookService = BookService();
+  final ProfileService _profileService = ProfileService();
+  final BookReadingService _bookReadingService = BookReadingService();
+
+  late Future<Map<String, dynamic>> _readerConfig;
+
+  @override
+  void initState() {
+    super.initState();
+    _readerConfig = _initializeReader();
+  }
+
+  Future<Map<String, dynamic>> _initializeReader() async {
+    final file = await _bookService.getBookFile(widget.book);
+    final startCfi = await _bookReadingService.loadReadingPosition(widget.book.id);
+    final config = await _profileService.getAllConfigurations();
+
+    return {
+      'file': file,
+      'startCfi': startCfi,
+      'config': config,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WebviewReader(book: widget.book);
+    return FutureBuilder<Map<String, dynamic>>(
+      future: _readerConfig,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          final data = snapshot.data!;
+          return WebviewReader(
+            book: widget.book,
+            file: data['file'],
+            startCfi: data['startCfi'],
+            config: data['config'],
+          );
+        } else {
+          return const Center(child: Text('No data available'));
+        }
+      },
+    );
   }
 }
--- lib/widgets/webview_reader/webview_reader.dart
+++ lib/widgets/webview_reader/webview_reader.dart
@@ -2,60 +2,44 @@
  * 基于 Epub.js 实现的阅读器，被 lib/epub_viewer 再次封装
  */
 import 'dart:io';
-import 'dart:convert';
-import 'package:enladder_mobile/services/profile_service.dart';
-import 'package:enladder_mobile/widgets/webview_reader/word_lookup_dialog.dart';
-import 'package:path_provider/path_provider.dart';
-
-import 'package:enladder_mobile/models/book.dart';
-import 'package:enladder_mobile/services/book_service.dart';
 import 'package:enladder_mobile/services/book_reading_service.dart';
-import 'package:enladder_mobile/widgets/webview_reader/chapter_drawer.dart';
-import 'package:enladder_mobile/widgets/webview_reader/search_page.dart';
+import 'package:enladder_mobile/widgets/webview_reader/word_lookup_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_epub_viewer/flutter_epub_viewer.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:enladder_mobile/models/book.dart';
+import 'package:enladder_mobile/widgets/webview_reader/chapter_drawer.dart';
+import 'package:enladder_mobile/widgets/webview_reader/search_page.dart';
 
 class WebviewReader extends StatefulWidget {
-  WebviewReader({Key? key, required this.book}) : super(key: key);
+  const WebviewReader({
+    Key? key,
+    required this.book,
+    required this.file,
+    required this.startCfi,
+    required this.config,
+  }) : super(key: key);
 
   final Book book;
+  final File file; // The EPUB file
+  final String? startCfi; // The starting CFI for reading
+  final Map<String, dynamic> config; // Configuration settings
 
   @override
   State<StatefulWidget> createState() => _WebviewReaderState();
 }
 
 class _WebviewReaderState extends State<WebviewReader> {
-  final bookService = BookService();
-  final profileService = ProfileService();
-  final bookReadingService = BookReadingService();
-  final epubController = EpubController();
+  final EpubController epubController = EpubController();
   final FlutterTts flutterTts = FlutterTts();
+  final bookReadingService = BookReadingService();
   String textSelection = '';
   bool isLoading = true;
-  // 用于展示进度
   double progress = 0.0;
-
-  late Future<Map<String, dynamic>> combinedFuture;
 
   @override
   void initState() {
     super.initState();
-    combinedFuture = _initializeReader(); // Combine the futures
-  }
-
-  Future<Map<String, dynamic>> _initializeReader() async {
-    final file = await bookService.getBookFile(widget.book);
-    final startCfi = await bookReadingService.loadReadingPosition(widget.book.id);
-    final config = await profileService.getAllConfigurations();
-    
-    print(config);
-    
-    return {
-      'file': file,
-      'startCfi': startCfi,
-      'config': config
-    };
   }
 
   @override
@@ -72,63 +56,39 @@ class _WebviewReaderState extends State<WebviewReader> {
             icon: const Icon(Icons.search),
             onPressed: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => SearchPage(
-                            epubController: epubController,
-                          )));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SearchPage(
+                    epubController: epubController,
+                  ),
+                ),
+              );
             },
           ),
         ],
       ),
       body: SafeArea(
-        child: FutureBuilder<Map<String, dynamic>>(
-          future: combinedFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              final file = snapshot.data!['file'] as File;
-              final startCfi = snapshot.data!['startCfi'] as String?;
-              final config = snapshot.data!['config'] as Map<String, dynamic>;
-              return Column(
-                children: [
-                  LinearProgressIndicator(
-                    value: progress,
-                    backgroundColor: Colors.transparent,
-                  ),
-                  Expanded(
-                    child: Stack(
-                      children: [
-                        _buildEpubViewer(file, startCfi, config),
-                        if (isLoading)
-                          const Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
+        child: Stack(
+          children: [
+            _buildEpubViewer(),
+            if (isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildEpubViewer(File file, String? startCfi, Map<String, dynamic> config) {
-    print("Open book ${file.path} cfi: ${startCfi}");
-    
+  Widget _buildEpubViewer() {
     return EpubViewer(
-      epubSource: EpubSource.fromFile(file), // 使用文件路径打开
+      epubSource: EpubSource.fromFile(widget.file),
       epubController: epubController,
-      initialCfi: startCfi,
+      initialCfi: widget.startCfi,
       displaySettings: EpubDisplaySettings(
-        fontSize: config['fontSize'],
-        theme: config['theme'],
+        fontSize: widget.config['fontSize'],
+        theme: widget.config['theme'],
         flow: EpubFlow.paginated,
         snap: true,
         allowScriptedContent: false,
@@ -144,7 +104,7 @@ class _WebviewReaderState extends State<WebviewReader> {
           isLoading = false;
         });
       },
-      onEpubLoaded: () async {
+      onEpubLoaded: () {
         setState(() {
           isLoading = false;
         });
